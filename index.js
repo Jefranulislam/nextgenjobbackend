e54b89f5df6a62ef4339717b06a0413f8e154762
jefranulislam <jefranulislam@gmail.com>
2023-12-19 12:44:09 +0600

    fixed issues

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://rakibjefranulislam:5r5kYNrNQnlWdRic@clusterone.sr3eetu.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.URI;
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -25,9 +24,6 @@ async function run() {
     const jobsCollection = client.db("jobsCollection").collection("jobs");
     const userCollection = client.db("userCollection").collection("user");
     await client.connect();
-    
-    
-    
     app.get("/jobs", async (req, res) => {
       try {
 
@@ -55,6 +51,9 @@ async function run() {
       }
     });
 
+
+
+
     app.post("/jobs", async (req, res) => {
       try {
         const jobInfo = req.body;
@@ -74,65 +73,89 @@ async function run() {
         res.status(500).send("Server error");
       }
     });
-  
-    app.put("/jobs/:id", async (req, res) => {
-      try {
-        const jobId = req.params.id;
-        const updatedJobInfo = req.body;
-    
-        const result = await jobsCollection.updateOne(
-          { _id: ObjectId(jobId) }, 
-          { $set: updatedJobInfo }
-        );
-    
-        if (result.modifiedCount === 1) {
-          return res.status(200).json({
-            message: "Job Updated",
-            jobId: jobId,
+            
+          app.put("/jobs/:id", async (req, res) => {
+            try {
+              const jobId = req.params.id;
+              const updatedJobInfo = req.body;
+
+              const result = await jobsCollection.updateOne(
+                { _id: new ObjectId(jobId) },
+                {
+                  $set: updatedJobInfo,
+                  $push: { applications: updatedJobInfo.applicationData } 
+                }
+              );
+
+              if (result.modifiedCount === 1) {
+                return res.status(200).json({
+                  message: "Job Updated",
+                  jobId: jobId,
+                });
+              } else {
+                return res.status(404).json({ error: "Job not found or not updated" });
+              }
+            } catch (error) {
+              console.error(error);
+              res.status(500).send("Server error");
+            }
           });
-        } else {
-          return res.status(404).json({ error: "Job not found or not updated" });
-        }
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Server error");
-      }
-    });
 
 
 
+app.put('/signin/:email', async (req,res)=>{
 
-      app.post('/signin', async (req, res) => {
+try{
+  const userInfo = req.body;
+  
+  const result = await userCollection.insertOne({ userInfo});
+  if (result.insertedCount === 1) {
+    return res
+      .status(201)
+      .json({
+        message: "User updated successfully",
+        userInfo: result.insertedId,
+      });
+  }
+  res.send(result);
+}
+catch (error){
+console.log(error);
+}
+
+})
+
+
+      //Signup 
+      app.put('/api/signup', async (req, res) => {
         try {
-         const { name, email, password, role } = req.body;
-         const result = await userCollection.insertOne({ name, email, password, role });
-         if (result.insertedCount === 1) {
-           return res
-             .status(201)
-             .json({
-               message: "User created successfully",
-               jobId: result.insertedId,
-             });
-         }
-         res.send(result);
-         console.log(req.body);
-         res.json({status: 'ok'})
-          // const accessToken = jwt.sign({ name, email, password, role }, process.env.ACCESS_TOKEN);
-        // res.send(accessToken);
+          const  userInfo  = req.body;
+      
+        const result = await userCollection.insertOne({ userInfo});
+        if (result.insertedCount === 1) {
+          return res
+            .status(201)
+            .json({
+              message: "User updated successfully",
+              userInfo: result.insertedId,
+            });
+        }
+        res.send(result);
+        res.send(result._id);
+        console.log(result);
         } catch (error) {
           res.status(500).json({ error: error.message });
+          console.log(error);
         }
-      }
-      );
-
+      });
+      
+      
       //Signup 
       app.post('/api/signup', async (req, res) => {
         try {
           const { name, email, password, role } = userInfo; 
           userInfo = req.body;
-          // const accessToken = jwt.sign({ name, email, password, role}, process.env.ACCESS_TOKEN);
-          // res.json({accessToken});
-        // res.send(accessToken);
+      
         const result = await userCollection.insertOne({ name, email, password, role });
         if (result.insertedCount === 1) {
           return res
@@ -166,6 +189,9 @@ async function run() {
 run();
 
 app.get("/", (req, res) => {
+  res.send("WELCOME TO NEXTGENJOB");
+});
+app.get("/next", (req, res) => {
   res.send("WELCOME TO NEXTGENJOB");
 });
 app.listen(port, () => {
